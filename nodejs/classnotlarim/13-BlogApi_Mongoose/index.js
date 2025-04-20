5ff2a1011d9bf39002805a0318bc387fa1cabbc0
Ferhat Yigit <yiitferhat@gmail.com>
2025-04-20 17:47:45 +0300

    up

---
+++ nodejs/classnotlarim/13-BlogApi_Mongoose/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//! -------------------------------------------------------------------------- */
+//*                   EXPRESS JS - BLOG Project with Mongoose                  */
+//! -------------------------------------------------------------------------- */
+
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+//! Parse data
+app.use(express.json());
+
+//! Caach async errors
+require("express-async-errors");
+
+/* -------------------------------------------------------------------------- */
+//? Routes
+/* -------------------------------------------------------------------------- */
+app.all("/", (req, res) => res.send("Welcome to Blog API"));
+
+
+
+
+
+app.use(require("./src/middlewares/errorHandler"));
+/* -------------------------------------------------------------------------- */
+app.listen(PORT, () => console.log(`Running at http://127.0.0.1:` + PORT))
