04f3f9c77ce4eab9bf7c34adbfa95d663bc17cb5
Ferhat Yigit <yiitferhat@gmail.com>
2025-04-20 23:19:39 +0300

    up

--- nodejs/classnotlarim/13-BlogApi_Mongoose/index.js
+++ nodejs/classnotlarim/13-BlogApi_Mongoose/index.js
@@ -1,37 +1,37 @@
 "use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
 
-//! -------------------------------------------------------------------------- */
-//*                   EXPRESS JS - BLOG Project with Mongoose                  */
-//! -------------------------------------------------------------------------- */
-
-
-const express = require("express");
+const express = require('express');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
-//! Parse data
+// Parse data
 app.use(express.json());
 
-//! Caach async errors
-require("express-async-errors");
+// Cath async errors
+require('express-async-errors');
 
-// //? DB Connection
-// const dbConnection = require("./src/dbConnection");
+// DB Connection
+// const dbConnection = require('./src/dbConnection');
 // dbConnection();
-require("./src/dbConnection")();
+require('./src/dbConnection')();
 
-/* -------------------------------------------------------------------------- */
-//? Routes
-/* -------------------------------------------------------------------------- */
-app.all("/", (req, res) => res.send("Welcome to Blog API"));
+/*------------------------------------------------------- */
+// Main Route:
+app.all('/', (req, res) => res.send('Welcome to Blog API'));
 
-app.use(require("./src/routes/blog.router"))
-
-
+// Blog Route
+app.use(require('./src/routes/blog.router'));
 
 
-app.use(require("./src/middlewares/errorHandler"));
-/* -------------------------------------------------------------------------- */
-app.listen(PORT, () => console.log(`Running at http://127.0.0.1:` + PORT))
+/*------------------------------------------------------- */
+
+// Error Handler:
+app.use(require('./src/middlewares/errorHandler'));
+/*------------------------------------------------------- */
+
+app.listen(PORT, () => console.log('Running at http://127.0.0.1:' + PORT));
