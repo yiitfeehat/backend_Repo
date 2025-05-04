e2610c1aff8ecadf6ae2edcc39163904bebee22b
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 00:35:00 +0300

    token  model controller router up

--- nodejs/classnotlarim/20-PizzaApi/src/models/token.js
+++ nodejs/classnotlarim/20-PizzaApi/src/models/token.js
@@ -4,3 +4,20 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const TokenSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+    },
+    token: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+
+}, { collection: "tokens" });
+
+module.exports = mongoose.model("Token", TokenSchema);
--- nodejs/classnotlarim/20-PizzaApi/src/routes/token.js
+++ nodejs/classnotlarim/20-PizzaApi/src/routes/token.js
@@ -3,7 +3,14 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const router = require('express').Router()
+const { list, create, read, deletee} = require('../controllers/token')
 /* ------------------------------------------------------- */
+router.route('/')
+    .get(list) // List Users
+    .post(create) // Create User
 
+router.route('/:id')
+    .get(read) // Get User by ID
+    .delete(deletee) // Delete User by ID
 /* ------------------------------------------------------- */
 module.exports = router
