66cf60320c7186c83c8ab23563049e8122bfa1e3
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 00:19:47 +0300

    fix router vs

--- nodejs/classnotlarim/20-PizzaApi/src/models/user.js
+++ nodejs/classnotlarim/20-PizzaApi/src/models/user.js
@@ -3,46 +3,46 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
-const passwordEncryption = require('../helpers/passwordEncryption')
+const passwordEncryption = require('../helpers/passwordEncrypt')
 /* ------------------------------------------------------- */
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        set:(password)=>passwordEncryption(password)
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        set: (password) => passwordEncryption(password)
         // Validate process is done in controller
 
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        validate:{
-            validator:(email)=>{
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: (email) => {
                 return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
             },
-            message:"Email format is not valid!"
+            message: "Email format is not valid!"
         }
     },
-    isActive:{
-        type:Boolean,
-        default:true
+    isActive: {
+        type: Boolean,
+        default: true
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
+    isAdmin: {
+        type: Boolean,
+        default: false
     },
 
 
 
-},{collection:"users", timestamps:true});
+}, { collection: "users", timestamps: true });
 
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
