b3d0d6e4362fdea7343ea0f879d823a8659749ad
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 02:46:00 +0300

    add topping model controller router

--- nodejs/classnotlarim/20-PizzaApi/src/models/topping.js
+++ nodejs/classnotlarim/20-PizzaApi/src/models/topping.js
@@ -4,3 +4,20 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const ToppingSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true
+        },
+        calories: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    },
+    { collection: "toppings", timestamps: true }
+);
+module.exports = mongoose.model("Topping", ToppingSchema);
