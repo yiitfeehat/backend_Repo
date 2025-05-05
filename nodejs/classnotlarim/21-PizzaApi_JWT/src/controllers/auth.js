b009e9698700db4294df8f15595b02df0e4b0c23
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 17:24:57 +0300

    add authentication controller and routes; implement custom error handling

--- nodejs/classnotlarim/21-PizzaApi_JWT/src/controllers/auth.js
+++ nodejs/classnotlarim/21-PizzaApi_JWT/src/controllers/auth.js
@@ -1,4 +1,53 @@
 "use strict"
+
+const CustomError = require("../helpers/customError");
+
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
+
+module.exports = {
+    login: async (req, res) => {
+
+        /*
+           #swagger.tags = ["Authentication"]
+           #swagger.summary = "Login"
+           #swagger.description = 'Login with username (or email) and password for get simpleToken and JWT'
+           #swagger.parameters["body"] = {
+               in: "body",
+               required: true,
+               schema: {
+                   "username": "test",
+                   "password": "aA12345.?",
+               }
+           }
+       */
+
+        const { username, email, password } = req.body;
+
+        if (!((email || username) && password)) {
+            // res.ErrorStatusCode = 401;
+            // throw new Error("username/email and password are required")
+            throw new CustomError("username/email and password are required", 401)
+        }
+
+        res.status(200).send({
+            error: false,
+            message: "ok kanka"
+        })
+    },
+    logout: async (req, res) => {
+
+        res.status(200).send({
+            error: false,
+            message: "ok kanka"
+        })
+    },
+    refresh: async (req, res) => {
+
+        res.status(200).send({
+            error: false,
+            message: "ok kanka"
+        })
+    }
+}
--- nodejs/classnotlarim/21-PizzaApi_JWT/src/routes/auth.js
+++ nodejs/classnotlarim/21-PizzaApi_JWT/src/routes/auth.js
@@ -3,7 +3,15 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const router = require('express').Router()
+const {login, logout, refresh} = require("../controllers/auth")
+
 /* ------------------------------------------------------- */
+// URL indexde /auth yazılmış o yüzden burda yazmayacağız.
+
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("refresh", refresh)
+
 
 /* ------------------------------------------------------- */
 module.exports = router
