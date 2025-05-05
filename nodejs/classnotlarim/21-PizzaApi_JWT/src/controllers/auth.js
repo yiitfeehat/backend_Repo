06fd2e92cd9331e5113b7be0ff525f69a777e29e
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 18:17:50 +0300

    jwt added

--- nodejs/classnotlarim/21-PizzaApi_JWT/src/controllers/auth.js
+++ nodejs/classnotlarim/21-PizzaApi_JWT/src/controllers/auth.js
@@ -1,6 +1,10 @@
 "use strict"
 
 const CustomError = require("../helpers/customError");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+const Token = require("../models/token");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken")
 
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
@@ -31,23 +35,63 @@ module.exports = {
             throw new CustomError("username/email and password are required", 401)
         }
 
+        const user = await User.findOne({ $or: [{ email }, { username }], password });
+        if (!user) throw new CustomError("Incorrect email/username or password", 401)
+        if (!user.isActive) throw new CustomError("This accounts is not active")
+
+        //! --------------Simple Token Version--------------------
+        let tokenData = await Token.findOne({ userId: user._id });
+
+        if (!tokenData) {
+            tokenData = await Token.create({
+                userId: user._id,
+                token: passwordEncrypt(Date.now() + user._id)
+            })
+        }
+
+        //! ------------------------------------------------------
+        /* -------------------------------------------------------------------------- */
+        //* JWT TOKEN---------------------------------------------
+        // Access Token:
+        const accessData = {
+            _id: user._id,
+            username: user.username,
+            isActive: user.isActive,
+            isAdmin: user.isAdmin
+        };
+
+        // const accessToken = jwt.sign(payload, secretKey, {lifetime})
+        const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '15m' });
+
+        // Refresh Token:
+        const refreshToken = jwt.sign({ _id: user._id }, process.env.REFRESH_KEY, { expiresIn: '1d' });
+        //*-------------------------------------------------------
+
         res.status(200).send({
             error: false,
-            message: "ok kanka"
+            baerer: {
+                access: accessToken,
+                refresh: refreshToken
+            },
+            message: "ok kanka girdin"
         })
     },
+
+
+
+
     logout: async (req, res) => {
 
         res.status(200).send({
             error: false,
-            message: "ok kanka"
+            message: "ok kanka çıktın"
         })
     },
     refresh: async (req, res) => {
 
         res.status(200).send({
             error: false,
-            message: "ok kanka"
+            message: "ok kanka refreshledin"
         })
     }
 }
