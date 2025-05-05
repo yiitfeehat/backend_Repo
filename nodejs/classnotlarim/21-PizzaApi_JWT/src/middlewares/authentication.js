985b71e65502458a021b9ffdc1583aae1e6902ac
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-05 20:09:50 +0300

    auth jwt refresh added. end of the session 21

--- nodejs/classnotlarim/21-PizzaApi_JWT/src/middlewares/authentication.js
+++ nodejs/classnotlarim/21-PizzaApi_JWT/src/middlewares/authentication.js
@@ -17,7 +17,7 @@ module.exports = async (req, res, next) => {
         const tokenData = await Token.findOne({ token: tokenArr[1] }).populate("userId")  // {userId : {...userInfo}, token} || null
 
         req.user = tokenData ? tokenData.userId : null;
-    } else if (tokenArr && tokenArr[0] == "Baerer") {
+    } else if (tokenArr && tokenArr[0] == "Bearer") {
         // jwt.verify(jwtToken, secretKey, callback func)
         jwt.verify(tokenArr[1], process.env.ACCESS_KEY, (error, accessData) => {
             // console.log(error);
