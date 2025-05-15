3cd1d5cbce64ab464a43166b83041b87e9a6a725
Ferhat Yigit <yiitferhat@gmail.com>
2025-05-15 12:03:02 +0300

    session 24 starter

---
+++ nodejs/classnotlarim/24-StockApi/_projectStarter/index.js
@@ -0,0 +1,72 @@
+"use strict"
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+const express = require('express');
+const app = express();
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+// envVariables to process.env:
+require('dotenv').config();
+const HOST = process.env?.HOST || '127.0.0.1'
+const PORT = process.env?.PORT || 8000
+
+app.set("query parser", "extended");
+/* ------------------------------------------------------- */
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require('./src/configs/dbConnection');
+dbConnection();
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json());
+
+// Check Authentication:
+app.use(require('./src/middlewares/authentication'));
+
+// Run Logger:
+app.use(require('./src/middlewares/logger'));
+
+// Query Handler:
+app.use(require('./src/middlewares/queryHandler'));
+
+/* ------------------------------------------------------- */
+// Routes:
+
+// HomePath:
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to Stock Management API',
+        documents: {
+            swagger: '/documents/swagger',
+            redoc: '/documents/redoc',
+            json: '/documents/json',
+        },
+        user: req.user
+    })
+});
+
+// Static Route:
+app.use('/upload', express.static('./upload'));
+
+// Routes:
+app.use(require('./src/routes'));
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')() // !!! It clear database.
---
+++ nodejs/classnotlarim/24-StockApi/_projectStarter/src/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+"use strict"
+/* -------------------------------------------------------
+    | FULLSTACK TEAM | NODEJS / EXPRESS |
+------------------------------------------------------- */
+// app.use(errorHandler):
+
+module.exports = (err, req, res, next) => {
+
+    return res.status( err.statusCode || res?.errorStatusCode || 500).send({
+        error: true,
+        message: err.message,
+        cause: err.cause,
+        body: req.body
+    });
+}
