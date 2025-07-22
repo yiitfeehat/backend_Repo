eed953624f801bdaf7b8560366c33ef5bc9f18aa
Ferhat Yigit <yiitferhat@gmail.com>
2025-07-23 00:24:17 +0300

    se24 - All Routers Done

--- nodejs/classnotlarim/24-StockApi/src/routes/index.js
+++ nodejs/classnotlarim/24-StockApi/src/routes/index.js
@@ -4,30 +4,27 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
-// routes/:
 
-// URL: /
-
-// // auth:
+// auth:
 // router.use('/auth', require('./auth'))
-// // user:
-// router.use('/users', require('./user'))
-// // token:
-// router.use('/tokens', require('./token'))
+// user:
+router.use('/users', require('./user'))
+// token:
+router.use('/tokens', require('./token'))
 
 
-// // brand:
-// router.use('/brands', require('./brand'))
-// // category:
-// router.use('/categories', require('./category'))
-// // firm:
-// router.use('/firms', require('./firm'))
-// // product:
-// router.use('/products', require('./product'))
-// // purchase:
-// router.use('/purchases', require('./purchase'))
-// // sale:
-// router.use('/sales', require('./sale'))
+// brand:
+router.use('/brands', require('./brand'))
+// category:
+router.use('/categories', require('./category'))
+// firm:
+router.use('/firms', require('./firm'))
+// product:
+router.use('/products', require('./product'))
+// purchase:
+router.use('/purchases', require('./purchase'))
+// sale:
+router.use('/sales', require('./sale'))
 
 // document:
 router.use('/documents', require('./document'))
--- nodejs/classnotlarim/24-StockApi/src/routes/product.js
+++ nodejs/classnotlarim/24-StockApi/src/routes/product.js
@@ -4,3 +4,11 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+
+const { list, create, read, update, deletee } = require("../controllers/product");
+
+router.route("/").get(list).post(create);
+
+router.route("/:id").get(read).put(update).patch(update).delete(deletee);
+
+module.exports = router;
